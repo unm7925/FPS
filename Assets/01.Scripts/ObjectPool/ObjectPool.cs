e7d04a0f49db427b5baae850d581353df28999a0
leejihoon <unm7925@gmail.com>
2026-03-23 00:11:46 +0900

    feat : objectpool

---
+++ Assets/01.Scripts/ObjectPool/ObjectPool.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool<T> where T : Component
+{
+        private Queue<T> objectPool;
+
+        public T Get()
+        {
+            T go = objectPool.Dequeue();
+            go.gameObject.SetActive(true);
+            return go;
+        }
+
+        public void Return(T obj)
+        {
+                obj.gameObject.SetActive(false);
+                objectPool.Enqueue(obj);
+        }
+
+        public void Init(T prefab, int count, Transform parent)
+        {
+            objectPool = new Queue<T>();
+            
+            for (int i = 0; i < count; i++) 
+            {
+                T go = GameObject.Instantiate(prefab,parent);
+                go.gameObject.SetActive(false);
+                objectPool.Enqueue(go);
+            }
+        }
+}
+
